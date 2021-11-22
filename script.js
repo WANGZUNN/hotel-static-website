6642034f3d3f675f7ce5bdf8bdf058c567e0b38e
VENG SRUN <90465145+WANGZUNN@users.noreply.github.com>
2021-11-22 22:56:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function over () {
+    text.innerHTML = 'Make your dream come true';
+    text.style.fontSize= '5rem';
+    text.style.color='white';
+    text.style.font="Arial, Helvetica, sans-serif";
+}
+
+function out () {
+    text.innerHTML = 'Travel Beyond Limits.';
+    text.style.fontSize= '5rem';
+    text.style.color='white';
+    text.style.font="Arial, Helvetica, sans-serif";
+}
+
+text.addEventListener("mouseover", over);
+text.addEventListener("mouseout",out);
+
+
+
+
